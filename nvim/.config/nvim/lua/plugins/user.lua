ce6dddf8c26ba56f861d7fe9d538a49beb9e67a5
Ömer Düzgün <cecemaru@hotmail.com>
2026-04-29 10:00:02 +0300

    chore(nvim): remove AstroNvim distribution
    Drop the AstroNvim preset to make room for a fully manual,
    plugin-by-plugin configuration.

--- nvim/.config/nvim/lua/plugins/user.lua
+++
@@ -1,9 +0,0 @@
-return {
- {
-    "augmentcode/augment.vim",
-    lazy = false,
-    config = function()
-      vim.g.augment_workspace_folders = { "/Users/omerduzgun/Documents/nuxt-ui" }
-    end,
-  },
-}
